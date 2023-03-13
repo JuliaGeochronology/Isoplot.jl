66b44fd095bf9c5bb48eb057d6f32800a78dff8d
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 04:32:36 +0000

    build based on 7c6924b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Isoplot","category":"page"},{"location":"#Isoplot","page":"Home","title":"Isoplot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Isoplot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Isoplot]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Isoplot","category":"page"},{"location":"#Isoplot","page":"Home","title":"Isoplot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Isoplot.jl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Isoplot]","category":"page"}]
 }
