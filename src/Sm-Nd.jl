0012c835ec0a4632e0af70ae785b6d9efa65a4a3
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-04-01 21:24:09 -0400

    Use Measurements.jl when converting t1/2 to λ

--- src/Sm-Nd.jl
+++ src/Sm-Nd.jl
@@ -1,3 +1,3 @@
-# Villa et al. (2020) 10.1016/j.gca.2020.06.022
-λ146Sm = 6.524-6 ± 0.024e-6/2 # 1/Myr
-export λ146Sm
+λ147Sm = 6.524-6 ± 0.024e-6/2 # 1/Myr, Villa et al. (2020) 10.1016/j.gca.2020.06.022
+λ146Sm = log(2)/(103.1 ± 4.5) # 1/Myr, Meissner et al. (1987)
+export λ147Sm, λ147Sm
