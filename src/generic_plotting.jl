68fac7841f9d97d7faa57875a3ae0d5406f5cc3c
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-06-09 22:11:34 -0400

    Avoid method redefinition in plotting extensions, to allow precompilaiton

--- src/generic_plotting.jl
+++ src/generic_plotting.jl
@@ -1,11 +1,16 @@
-function concordiacurve() end
+# Establish our plotting functions with dummy methods that will never be used
+# (using zero-arg methods for this isn't sufficient since concordialine 
+#  and concordialine! may have real zero-arg methods)
+struct NotUsed end
 
-function concordiacurve!() end
+function concordiacurve(x::NotUsed) end
 
-function concordialine() end
+function concordiacurve!(x::NotUsed) end
 
-function concordialine!() end
+function concordialine(x::NotUsed) end
 
-function rankorder() end
+function concordialine!(x::NotUsed) end
 
-function rankorder!() end
+function rankorder(x::NotUsed) end
+
+function rankorder!(x::NotUsed) end
