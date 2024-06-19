b14fc7a8e2eb7b76e298e0a78c1db0d4bba9eba1
Neven Sajko <s@purelymail.com>
2024-06-20 01:34:00 +0200

    delete unnecessary methods (#11)
    FTR, coming from
    https://discourse.julialang.org/t/method-overwriting-error-on-package-extensions/114630/

--- src/generic_plotting.jl
+++ src/generic_plotting.jl
@@ -1,16 +1,11 @@
-# Establish our plotting functions with dummy methods that will never be used
-# (using zero-arg methods for this isn't sufficient since concordialine 
-#  and concordialine! may have real zero-arg methods)
-struct NotUsed end
+function concordiacurve end
 
-function concordiacurve(x::NotUsed) end
+function concordiacurve! end
 
-function concordiacurve!(x::NotUsed) end
+function concordialine end
 
-function concordialine(x::NotUsed) end
+function concordialine! end
 
-function concordialine!(x::NotUsed) end
+function rankorder end
 
-function rankorder(x::NotUsed) end
-
-function rankorder!(x::NotUsed) end
+function rankorder! end
